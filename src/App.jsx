b95a9ccf5246e82437f59ee40e04076d74db8a08
SunJu <ciwrssn10@naver.com>
2026-05-10 21:09:41 +0900

    ✨ Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3>ECO-KNOCK</h3>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,38 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [                                  //Vite가 실행할 때 적용할 플러그인 목록
+    react(),
+    VitePWA({                                //PWA 기능 설정하기
+      registerType: 'autoUpdate',            // service worker 자동 업데이트
+      manifest: {
+        name: 'ECO-KNOCK',
+        short_name: 'ECO-KNOCK',
+        description: '에코놐',
+        theme_color: '#ffffff',
+        icons: [
+          { // 일단 임시로 채워둠
+            src: '/icons/glaham-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/glaham-icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png'
+          }
+        ],
+        display: 'standalone',  // 앱처럼 보임(상단 주소창 제거)
+        start_url: '/'          //앱이 실행될 때 처음 열릴 주소
+      }
+    })
+  ],
+  resolve: {                    // 절대경로 alias 설정
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
+})
