1d3021aeb6abbe8b067778da004f5f52cd6a58e3
SunJu <ciwrssn10@naver.com>
2026-05-14 03:00:02 +0900

    📝 Docs: Set page route variable

---
+++ src/pages/CollectionPage.jsx
@@ -0,0 +1,15 @@
+// pages/CollectionPage.jsx
+import './CollectionPage.css';
+
+const CollectionPage = () => {
+  return (
+    <div className="collection-page">
+      {/* 로고 */}
+      <h1 className="collection-page__logo">ECO-KNOCK</h1>
+
+      {/* 바로가기 컬렉션 그리드 넣기!! */}
+    </div>
+  );
+};
+
+export default CollectionPage;
