9bc2590c77f4c3c02ab1b938cf7e0f032bc61f27
SunJu <ciwrssn10@naver.com>
2026-07-19 15:08:50 +0900

    ♻️ Refactor: Share development notice of features

--- src/features/chat/constants/chatText.js
+++ src/features/chat/constants/chatText.js
@@ -4,7 +4,3 @@ export const PHOTO_ATTACH_LABEL = '사진 파일 첨부';
 export const FILE_ATTACH_LABEL = '파일 첨부';
 export const SEND_LABEL = '메시지 보내기';
 export const CHAT_PLACEHOLDER = '에코노 차은우가 누구야?';
-
-
-export const ATTACHMENT_NOTICE_MESSAGE = '추후 개발예정\n입니다 ^-^';
-export const ATTACHMENT_NOTICE_CONFIRM_LABEL = '확인';
---
+++ src/shared/constants/developmentNotice.js
@@ -0,0 +1,2 @@
+export const DEVELOPMENT_NOTICE_MESSAGE = '추후 개발예정\n입니다 ^-^';
+export const DEVELOPMENT_NOTICE_CONFIRM_LABEL = '확인';
