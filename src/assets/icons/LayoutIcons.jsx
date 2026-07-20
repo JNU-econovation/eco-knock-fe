e4a3f757686d077a6bdebd3cfe62ebbf548fd571
SunJu <ciwrssn10@naver.com>
2026-07-20 21:12:27 +0900

    💄 Style: Adjust back icon alignment

--- src/assets/icons/LayoutIcons.jsx
+++ src/assets/icons/LayoutIcons.jsx
@@ -1,5 +1,5 @@
 export const BackIcon = () => (
-  <svg width="10" height="16" viewBox="0 0 10 16" fill="none" aria-hidden="true">
+  <svg width="13" height="16" viewBox="0 0 13 16" fill="none" aria-hidden="true">
     <path
       d="M8.5 14.5 2 8l6.5-6.5"
       stroke="currentColor"
